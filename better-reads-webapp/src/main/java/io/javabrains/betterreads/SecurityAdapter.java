b6f9a78b3946a150cb7b56eb6756e7274a8bfdba
clun <cedrick.lunven@gmail.com>
2022-02-15 17:48:41 +0100

    fix csrf

--- better-reads-webapp/src/main/java/io/javabrains/betterreads/SecurityAdapter.java
+++ better-reads-webapp/src/main/java/io/javabrains/betterreads/SecurityAdapter.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -25,10 +26,10 @@ public class SecurityAdapter extends WebSecurityConfigurerAdapter {
 				.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
 			)
 			// Disabling security
-			.csrf().disable()
-			//.csrf(c -> c
-			//	.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-			//)
+			//.csrf().disable()
+			.csrf(c -> c
+				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+			)
 			.logout(l -> l
 			    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))            
                 .invalidateHttpSession(true)
